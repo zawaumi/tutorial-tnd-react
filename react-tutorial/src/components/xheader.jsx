be616d98b65b9eda75cfd70d8454d00b6ec5f3c0
KukimiCan <41055ht@gmail.com>
2025-05-30 13:07:34 +0900

    header部分のjsxおよびcssの作成 画像フォルダをpublicに追加

---
+++ react-tutorial/src/components/xheader.jsx
@@ -0,0 +1,25 @@
+import "./xheader.css";
+
+function xheader() {
+    return (
+        <>
+            <header id="header">
+                
+                <h1 id="site-title"><a href="App.jsx"><img id="site-logo" src="../../public/img/logo.svg" alt="" /></a></h1>
+
+                <nav>
+                    <ul id="nav-contents">
+                        <li><a id="nav-cont" href="#about">About</a></li>
+                        <li><a id="nav-cont" href="#works">Works</a></li>
+                        <li><a id="nav-cont" href="#news">News</a></li>
+                        <li><a id="nav-cont" href="#contact">Contact</a></li>
+                        <li><a href="https://www.instagram.com/"><img id="insta-logo" src="/img/icon-instagram.png" alt="" /></a></li>
+                    </ul>
+                </nav>
+                
+            </header>
+        </>
+    );
+}
+
+export default xheader;
