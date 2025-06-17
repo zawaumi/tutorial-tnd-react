e17904056546c36146c287a9def5aeee539a3cf1
KFugaku <kf314pdtomato@gmail.com>
2025-06-17 16:59:05 +0900

    ファイル構造の変更

--- react-tutorial/src/components/exhibit.jsx
+++ react-tutorial/src/components/exhibit.jsx
@@ -10,12 +10,12 @@ function exhibit(){
           <button class="scroll-btn left" onclick="scrollleft()">&#10094;</button>
           <div class="carousel" id="carousel">
             <div class="spacer"></div>
-            <img src="works1.jpg" alt="Photo 1"></img>
-            <img src="works2.jpg" alt="Photo 2"></img>
-            <img src="works3.jpg" alt="Photo 3"></img>
-            <img src="works4.jpg" alt="Photo 4"></img>
-            <img src="works5.jpg" alt="Photo 5"></img>
-            <img src="works6.jpg" alt="Photo 6"></img>
+            <img src="/img/works1.jpg" alt="Photo 1"></img>
+            <img src="/img/works2.jpg" alt="Photo 2"></img>
+            <img src="/img/works3.jpg" alt="Photo 3"></img>
+            <img src="/img/works4.jpg" alt="Photo 4"></img>
+            <img src="/img/works5.jpg" alt="Photo 5"></img>
+            <img src="/img/works6.jpg" alt="Photo 6"></img>
             <div class="spacer"></div>
           </div>
           <button class="scroll-btn right" onclick="scrollright()">&#10095;</button>
