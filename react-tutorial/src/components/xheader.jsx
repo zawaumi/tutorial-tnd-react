179842b96d6beeb9b5ff07b49928259e37ece0db
KukimiCan <41055ht@gmail.com>
2025-06-20 07:45:52 +0900

    途中まで

--- react-tutorial/src/components/xheader.jsx
+++ react-tutorial/src/components/xheader.jsx
@@ -1,19 +1,17 @@
-import "./xheader.css";
-
 export default function Xheader() {
     return (
         <>
-            <header id="header">
+            <header className="flex items-center justify-between max-w-[60.00rem] h-64 my-0 m-auto py-0 px-16 opacity-100;">
                 
-                <h1 id="site-title"><a href="App.jsx"><img id="site-logo" src="/img/logo.svg" alt="" /></a></h1>
+                <h1 className="block h-9;"><a href="App.jsx"><img className="w-28 h-9 max-w-full m-0 p-0;" src="/img/logo.svg" alt="" /></a></h1>
 
                 <nav>
-                    <ul id="nav-contents">
-                        <li><a id="nav-cont" href="#about">About</a></li>
-                        <li><a id="nav-cont" href="#works">Works</a></li>
-                        <li><a id="nav-cont" href="#news">News</a></li>
-                        <li><a id="nav-cont" href="#contact">Contact</a></li>
-                        <li><a href="https://www.instagram.com/"><img id="insta-logo" src="/img/icon-instagram.png" alt="" /></a></li>
+                    <ul className="flex w-80 h-7 m-0 py-2 px-0;">
+                        <li className=" ml-8;"><a className="no-underline text-[#24292E] m-0 p-0 text-left text-[0.9rem]2" href="#about">About</a></li>
+                        <li className=" ml-8;"><a className="text-[#24292E] m-0 p-0; text-left text-[0.90rem] leading-5" href="#works">Works</a></li>
+                        <li className=" ml-8;"><a className="text-[#24292E] m-0 p-0; text-left text-[0.90rem] leading-5" href="#news">News</a></li>
+                        <li className=" ml-8;"><a className="text-[#24292E] m-0 p-0; text-left text-[0.90rem] leading-5" href="#contact">Contact</a></li>
+                        <li className=" ml-8;"><a href="https://www.instagram.com/"><img className="w-5 h-5;" src="/img/icon-instagram.png" alt="" /></a></li>
                     </ul>
                 </nav>
                 
