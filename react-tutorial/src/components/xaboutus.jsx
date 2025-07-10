eaecc2a460b4e8dba680b52d8fb662c6912c70ca
KukimiCan <41055ht@gmail.com>
2025-07-10 15:08:03 +0900

    aboutus部分を作りました

---
+++ react-tutorial/src/components/xaboutus.jsx
@@ -0,0 +1,17 @@
+export default function Xaboutus(){
+    return (
+        <>
+        <section className="font-[Meiryo]">
+            <h2 className="text-2xl font-bold text-center m-14">About</h2>
+            <ul className="text-[0.9rem]">
+                <li className="mb-8">Xxxxx Ashley</li>
+                <li>2th Floor xxxxx Building x-x-x Nishiazabu, Minato-ku, Tokyo 106-0031 Japan</li>
+                <li>tel: 000-0000-0000</li>
+                <li>url: www.xxxxxx.jp</li>
+                <li className="mb-8">mail: xxx@xxxxxx.jp</li>
+            </ul>
+            <p className="text-[0.9rem]">プロフィールテキストテキストテキストテキストテキストテキストテキストテキストテキストストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+        </section>
+        </>
+    );
+}
